2923f36089cc97792f5a293f22ed01ad02fe6c7c
Timothy Andrew <mail@timothyandrew.net>
2012-07-14 21:01:29 +0530

    Use a helper function to get full <title> in spec.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,36 +1,33 @@
 require 'spec_helper'
 
 describe "Static pages" do
-
-  let(:base_title) { "Ruby on Rails Tutorial Sample App"}
   subject { page }
 
   describe "Home page" do    
     before { visit root_path }
 
     it { should have_selector 'h1', text: 'Sample App' }
-    it { should have_selector 'title', text: base_title }
-    it { should_not have_selector 'title', text: '| Home' }
+    it { should have_selector 'title', text: full_title('') }
   end
 
   describe "Help Page" do
     before { visit help_path }
 
     it { should have_selector 'h1', text: 'Help' }
-    it { should have_selector 'title', text: "#{base_title} | Help" }
+    it { should have_selector 'title', text: full_title('Help') }
   end
 
   describe "About page" do
     before { visit about_path }
 
     it { should have_selector 'h1', text: 'About Us' }
-    it { should have_selector 'title', text: "#{base_title} | About Us" }
+    it { should have_selector 'title', text: full_title('About Us') }
   end
 
   describe "Contact page" do
     before { visit contact_path }
 
     it { should have_selector 'h1', text: 'Contact Us' }
-    it { should have_selector 'title', text: "#{base_title} | Contact Us" }
+    it { should have_selector 'title', text: full_title('Contact Us') }
   end
 end
---
+++ spec/support/utilities.rb
@@ -0,0 +1,6 @@
+#Returns the full title on a per-page basis
+def full_title(page_title)
+  title = "Ruby on Rails Tutorial Sample App"
+  title << " | #{page_title}" unless page_title.empty?
+  title
+end

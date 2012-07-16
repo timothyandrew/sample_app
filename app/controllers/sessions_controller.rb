3a9fec202e1be9d5fbaf169dc31afeda5334280c
Timothy Andrew <mail@timothyandrew.net>
2012-07-16 23:16:58 +0530

    Add signin page that works for invalid input.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,13 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by_email(params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+      #Sign in
+    else
+      flash.now[:error] = 'Invalid email/password combination'
+      render 'new'
+    end
   end
 
   def destroy

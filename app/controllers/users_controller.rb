af9e37e26118f27a512324a74c03dcc38de82716
Timothy Andrew <mail@timothyandrew.net>
2012-07-16 08:25:29 +0530

    Add a basic signup form.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,13 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
 
+  def create
+    @user = User.new(params[:user])
+    unless @user.save
+      render 'new'
+    end
   end
 
   def show
